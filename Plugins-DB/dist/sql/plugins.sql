88f11c59ba3cd197c889f7506ee6d7b38309d6d3
UnAfraid <unafraid89@gmail.com>
2017-05-06 02:09:35 +0300

    Cleanup

--- Plugins-DB/dist/sql/plugins.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `plugins` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  `version` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
