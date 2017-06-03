17a5358990e0149a3d253230d090086a475c7419
UnAfraid <unafraid89@gmail.com>
2017-06-03 21:04:04 +0300

    Version 1.0.3 Fixing bug in guava https://github.com/google/guava/issues/2152 Minor update to eclipse templates

---
+++ Plugins-API/src/test/java/com/github/unafraid/plugins/ClassPathTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017 Rumen Nikiforov <unafraid89@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.unafraid.plugins;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.unafraid.plugins.installers.IPluginInstaller;
+import com.github.unafraid.plugins.util.ClassPathUtil;
+
+/**
+ * @author UnAfraid
+ */
+public class ClassPathTest
+{
+	@Test
+	public void pluginInstallerClassPath() throws Exception
+	{
+		final List<Class<IPluginInstaller>> classes = ClassPathUtil.getAllClassesExtending(IPluginInstaller.class);
+		Assert.assertNotEquals(classes.size(), 0);
+	}
+}
