409daddc3e20477a40be6c873ddb73ee1abad4b4
UnAfraid <unafraid89@gmail.com>
2019-07-29 23:54:11 +0300

    Version 1.1.4 Remove jars and IDE references plugins to avoid conflicts

--- Plugins-API/src/main/java/com/github/unafraid/plugins/PluginRepository.java
+++ Plugins-API/src/main/java/com/github/unafraid/plugins/PluginRepository.java
@@ -1,23 +1,23 @@
 /*
- * Copyright (c) 2017 Rumen Nikiforov <unafraid89@gmail.com>
+ *  Copyright (c) 2019 Rumen Nikiforov <unafraid89@gmail.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  */
 package com.github.unafraid.plugins;
 
@@ -121,6 +121,11 @@ public class PluginRepository<T extends AbstractPlugin> {
 			plugin.setJarHash(IDE_MODE);
 			
 			try {
+				final Map<String, T> plugins = this.plugins.get(plugin.getName());
+				if (!plugins.isEmpty()) {
+					LOGGER.info("Discarding plugins {} in favor of class-path registered ones", plugins);
+					plugins.clear();
+				}
 				processPlugin(plugin, Thread.currentThread().getContextClassLoader());
 			} catch (Exception e) {
 				LOGGER.warn("Failed to process plugin {}.", plugin, e);
@@ -257,6 +262,7 @@ public class PluginRepository<T extends AbstractPlugin> {
 		return plugins.values()
 				.stream()
 				.flatMap(map -> map.values().stream())
+				.distinct()
 				.sorted(Comparator.comparing(T::getJarHash).reversed());
 	}
 	
